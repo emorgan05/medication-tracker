59496d200a6bfa9e78d7e80ac390d76eb0537a25
Emily Morgan <emorgan05@gmail.com>
2017-11-14 19:30:48 +0000

    new medicine post route

--- app/controllers/medicine_controller.rb
+++ app/controllers/medicine_controller.rb
@@ -9,4 +9,13 @@ class MedicineController < ApplicationController
   get '/medicines/new' do
     erb :'medicines/new'
   end
+
+  post '/medicines' do
+    user = User.find(session[:id])
+    medicine = Medicine.new(params[:medicine])
+    medicine.user_id = user.id
+    medicine.save
+
+    redirect to '/medicines'
+  end
 end
