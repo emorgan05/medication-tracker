1fbe90bcde401f9f6303ecaac9fc320a49e45335
Emily Morgan <emorgan05@gmail.com>
2017-11-14 21:05:24 +0000

    add new method to calculate refill date

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,4 +1,9 @@
 class Medicine < ActiveRecord::Base
   belongs_to :user
 
+  def calculate_refill_date
+    num_of_days = self.number_in_bottle / self.dose_number
+    refill_date = DateTime.now.next_day(num_of_days)
+    refill_date.strftime("on %D")
+  end
 end
