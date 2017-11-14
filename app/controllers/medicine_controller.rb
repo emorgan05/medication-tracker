c428d768c5add372cf06fab009eb2195db7976a3
Emily Morgan <emorgan05@gmail.com>
2017-11-14 19:22:15 +0000

    add medicine index page and route. add new route and form.

--- app/controllers/medicine_controller.rb
+++ app/controllers/medicine_controller.rb
@@ -1,6 +1,12 @@
 class MedicineController < ApplicationController
 
   get '/medicines' do
+    @user = User.find(session[:id])
+
     erb :'medicines/index'
+  end
+
+  get '/medicines/new' do
+    erb :'medicines/new'
   end
 end
