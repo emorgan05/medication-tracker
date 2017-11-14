ba295c8e1b31036654222a9c895bae4c05ec85dc
Emily Morgan <emorgan05@gmail.com>
2017-11-14 19:51:31 +0000

    add user can select one medicine and see all details

--- app/controllers/medicine_controller.rb
+++ app/controllers/medicine_controller.rb
@@ -1,11 +1,13 @@
 class MedicineController < ApplicationController
 
+  #### show all medicines for the user
   get '/medicines' do
     @user = User.find(session[:id])
 
     erb :'medicines/index'
   end
 
+  #### user can create a new med
   get '/medicines/new' do
     erb :'medicines/new'
   end
@@ -18,4 +20,11 @@ class MedicineController < ApplicationController
 
     redirect to '/medicines'
   end
+
+  #### user can retrieve details for one medicine
+  get '/medicine/:id' do
+    @medicine = Medicine.find(params[:id])
+    erb :'medicines/show'
+  end
+
 end
