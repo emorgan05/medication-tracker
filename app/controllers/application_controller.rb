bd590e362ad96da2bfab54bfdbd708e3e8d5ab11
Emily Morgan <emorgan05@gmail.com>
2017-11-14 16:02:39 +0000

    ApplicationController. change set up and add helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,26 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
+    enable :sessions
+    set :session_secret, "secret"
     set :public_folder, 'public'
-    set :views, 'app/views'
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+    def self.current_user(session)
+      user = User.find(session[:id])
+    end
+
+    def logged_in?(session)
+      !!session[:id]
+    end
+  end
 end
