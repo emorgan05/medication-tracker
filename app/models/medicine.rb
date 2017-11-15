31eb0406a1491b8e4d9d48956800583dd2a3b282
Emily Morgan <emorgan05@gmail.com>
2017-11-15 16:33:13 +0000

    add #call_for_refill

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -5,4 +5,8 @@ class Medicine < ActiveRecord::Base
     num_of_days = self.number_in_bottle / self.dose_number
     refill_date = DateTime.now.next_day(num_of_days)
   end
+
+  def call_for_refill
+    call_date = self.refill_date.days_ago(5)
+  end
 end
