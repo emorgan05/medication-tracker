d6a8184169f81453d885340106f3bbe1ba1dc958
Emily Morgan <emorgan05@gmail.com>
2017-11-16 15:49:59 +0000

    add controller to create list of refill dates and show in calendar view

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,11 @@
 class CalendarController < ApplicationController
 
   get '/calendar' do
+    calendar = Calendar.find_by(user_id: session[:id])
+    dates = calendar.medicines.collect do |medicine|
+      medicine.refill_date
+    end
+    @dates = dates.sort
     erb :'calendars/index'
   end
 
